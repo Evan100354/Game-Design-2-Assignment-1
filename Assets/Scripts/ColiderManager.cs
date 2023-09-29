2236f94db126e9d5ad73df79a1f0cd72d627e6b7
PeterKavanagh97 <120379672+PeterKavanagh97@users.noreply.github.com>
2023-09-29 13:21:14 +0100

    Arthur did it

--- Assets/Scripts/ColiderManager.cs
+++ Assets/Scripts/ColiderManager.cs
@@ -8,13 +8,7 @@ public class ColiderManager : MonoBehaviour
     public PlayerParts playerParts;
     private void OnCollisionEnter2D(Collision2D other) 
     {
-        if(this.GetComponent<PlayerParts>().isCharging == true && other.gameObject.GetComponent<PlayerParts>().isCharging == true)
-        {
-            Debug.Log("SLAM!");
-            other.gameObject.GetComponent<Rigidbody2D>().velocity = (other.gameObject.GetComponent<Rigidbody2D>().velocity) * 5;
-            this.gameObject.GetComponent<Rigidbody2D>().velocity = (this.gameObject.GetComponent<Rigidbody2D>().velocity) * 5;
-        }
-        else if(other.gameObject.GetComponent<PlayerParts>().isCharging == true)
+        if(other.gameObject.GetComponent<PlayerParts>().isCharging == true)
         {
             Debug.Log("SHOVE!");
             other.gameObject.GetComponent<Rigidbody2D>().velocity = (-other.gameObject.GetComponent<Rigidbody2D>().velocity) / 2;
